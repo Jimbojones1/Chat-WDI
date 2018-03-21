873c298f4ba5d48ee667f10f877bf9a75af6732f
Jim Haff <Jimmyhaff3@gmail.com>
2018-03-20 20:29:53 -0500

    Update index.js

--- express-sockets/index.js
+++ express-sockets/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const chatServer = require('./socket-server')
+const chatServer = require('./socket-server');
 
 
 const server = app.listen(4000, ()=>{
-  console.log('server is listening on port 4000')
-})
+  console.log('server is listening on port 4000');
+});
 
 
 chatServer(server);
