fe780fb16075496fe3f665c287a5bd8e9296b8ef
Jim Haff <Jimmyhaff3@gmail.com>
2018-03-20 20:31:24 -0500

    Update index.js

--- socket-client/src/ChatRoom/index.js
+++ socket-client/src/ChatRoom/index.js
@@ -10,18 +10,18 @@ export default class ChatRoom extends Component {
     }
   }
   handleChange = (e) => {
-    this.setState({messageValue: e.currentTarget.value})
+    this.setState({messageValue: e.currentTarget.value});
   }
   handleSubmit = (e) =>{
     e.preventDefault();
     socket.emit('message', this.state.messageValue);
-    this.setState({messageValue: ''})
+    this.setState({messageValue: ''});
   }
   render(){
-    console.log(this.props.messages, ' this is this.props')
+    console.log(this.props.messages, ' this is this.props');
     const messages = this.props.messages.map((message, i) => {
       return <li key={i}>{message.username}: {message.message} </li>
-    })
+    });
 
     return (
       <div>
