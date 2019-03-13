8a81da282e330ec8eff0dc57c47c6d49fd2a4a7f
Jim Haff <Jimmyhaff3@gmail.com>
2019-03-13 11:09:44 -0500

    setup connection preparing folders

--- socket-client-hooks/src/App.js
+++ socket-client-hooks/src/App.js
@@ -1,8 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+export const socket = io.connect('http://localhost:4000');
+
 
 const App = () => {
+
+  
+
+  useEffect(() => {
+
+    socket.emit('something', 'taco')
+    console.log(socket, ' this is sockete')
+    socket.on('receive message', (data) => {
+      console.log(data)
+      console.log('hitting')
+    })
+  })
 
   return (
     <div>Hi</div>
