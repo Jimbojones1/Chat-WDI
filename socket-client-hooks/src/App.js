706e9ca4d53229e656e6b24a157bdf326c000ede
Jim Haff <Jimmyhaff3@gmail.com>
2019-03-13 16:07:12 -0500

    refactored into one component added css to app.css

--- socket-client-hooks/src/App.js
+++ socket-client-hooks/src/App.js
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import io from 'socket.io-client';
 import Login from './Login';
-import ChatBoard from './ChatBoard';
+import Rooms from './Rooms';
+import Users from './Users';
+import ChatRoom from './ChatRoom';
 export const socket = io.connect('http://localhost:4000');
 
 
@@ -11,18 +13,46 @@ const App = () => {
 
   const [username, setUsername] = useState('');
   const [isLogged, setLogged] = useState(false);
+  const [usernames, setUsernames] = useState([]);
+  const [messages, setMessages] = useState([]);
+  const [rooms, setRooms] = useState([]);
+  const [chatRoomName, setChatRoomName] = useState('');
 
   useEffect(() => {
+    console.log('something happening', socket)
+    socket.on('users', (usernames, roomname) => {
+      console.log(usernames, roomname, 'users')
+      setUsernames(usernames);
+      setChatRoomName(roomname);
+    });
 
-    socket.on('receive message', (data) => {
-      console.log(data)
-      console.log('hitting')
-    })
-  });
+    socket.on('messages', (messages) => {
+      console.log(messages, 'messages')
+      setMessages(messages)
+    });
+
+    socket.on('rooms', (rooms) => {
+        console.log(rooms, 'rooms');
+        setRooms(rooms);
+    });
+
+  }, [usernames, setChatRoomName, rooms, messages]);
+
+
 
   return (
     <div>
-      {isLogged ?  <ChatBoard /> :  <Login setUsername={username => setUsername(username)} setLogged={logged => setLogged(logged)}/>}
+      {isLogged ?
+
+      <div className="wrapper">
+       <Users usernames={usernames}/>
+       <Rooms rooms={rooms}/>
+       <ChatRoom messages={messages} chatroom={chatRoomName}/>
+      </div>
+
+      :
+
+       <Login setUsername={username => setUsername(username)} setLogged={logged => setLogged(logged)}/>}
     </div>
     )
 
